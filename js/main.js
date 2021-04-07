7c585c2bb87c0ddd19d01c38135213d042280de1
JuliusMatadamas <julius3877@gmail.com>
2021-04-06 19:39:15 -0500

    se modifica la seccion home del sitio.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 const 	navMenu 	= document.querySelector("#nav-menu"),
 		toggleMenu  = document.querySelector("#nav-toggle"),
 		closeMenu   = document.querySelector("#nav-close"),
-		navLink		= document.querySelectorAll(".nav__link");
+		navLink		= document.querySelectorAll(".nav__link"),
+		sections 	= document.querySelectorAll("section[id]");
 
+
+window.addEventListener('scroll', scrollActive);
 
 toggleMenu.addEventListener('click', ()=>{
 	navMenu.classList.toggle('show')
@@ -12,9 +15,29 @@ closeMenu.addEventListener('click', ()=>{
 	navMenu.classList.remove('show')
 });
 
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
 
 function linkAction(){
 	navMenu.classList.remove('show')
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+
+function scrollActive(){
+	const scrollY = window.pageYOffset;
+
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 50;
+		sectionId = current.getAttribute('id');
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active');
+		}
+		else {
+			document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active');
+		}
+	});
+}
+
 
